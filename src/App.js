4445acc6fe6c35c5c1f55cf5c0c6f2deed225cb5
51p50x <51plog50@gmail.com>
2022-03-20 23:33:06 -0600

    img2: Added Call to the API and also a .env.sample so you can use it as example to store your API key to unsplash

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import {Formik, Form, Field} from 'formik'
 import './header.css'
 
 const App = () =>{
+  const [photos, setPhotos] = useState([])
+  console.log({photos})
   return (
     <div>
       <header>
@@ -9,7 +12,14 @@ const App = () =>{
           initialValues = {{ search: ''}}
           onSubmit = {async values =>{
             //call to the api
-            console.log(values)
+            const response = await fetch(
+              `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,{
+                headers:{
+                  'Authorization': `Client-ID ${process.env.REACT_APP_UNSPLASH_KEY}`
+                }
+              })
+            const data = await response.json()
+            console.log(data)
           }}
         >
           <Form>
